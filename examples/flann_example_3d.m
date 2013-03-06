61cd44f1fc45d9c1602c3dc953b3e4d27e2de0d8
Roman <r.marchant@acfr.usyd.edu.au>
2013-03-07 09:50:18 +1100

    Data on 3D mesh. 30 million max points.

--- examples/flann_example_3d.m
+++ examples/flann_example_3d.m
@@ -1,39 +1,52 @@
-resolution = 0.005;
-[datasetX datasetY] = meshgrid(0:resolution:1,0:resolution:1);
-dataset = [datasetX(:)';datasetY(:)'];
-testset = [0.50001 0.5001]';
-figure;
-plot(dataset(1,:),dataset(2,:),'go');
-hold on;
-plot(testset(1,:),testset(2,:),'ro');
-axis ([0 1 0 1 0 1]);
+make_plot = 0; %%Make plot of results or not
 
+number_of_data_points = 1e6;% Works for Linear but not with kdtree 2e7;
+resolution = number_of_data_points^(1/3);
+max = 1;
+min = 0;
+[datasetX datasetY datasetZ] = meshgrid(min:(max-min)/resolution:max,min:(max-min)/resolution:max,min:(max-min)/resolution:max);
+if(size(datasetX,1)*size(datasetX,2)*size(datasetX,3)>3e7)
+    error('Too much data!');
+end
+dataset = [datasetX(:)';datasetY(:)';datasetZ(:)'];
+%randomize the order of dataset
+randomIndexes = randsample(size(dataset,2),size(dataset,2));
+dataset = dataset(:,randomIndexes);
+    
+testset = [0.5001;0.5001;0.5001];%rand(3,2);
+if(make_plot)
+    figure;
+    plot3(dataset(1,:),dataset(2,:),dataset(3,:),'go');
+    hold on;
+    plot3(testset(1,:),testset(2,:),testset(3,:),'ro');
+    axis ([0 1 0 1 0 1]);
+end
 %%
-tic;
-build_params.algorithm = 'kdtree';
+
+build_params.algorithm = 'linear';
 build_params.trees = 1;
 flann_set_distance_type(1);
-%build_params.target_precision = 0.7;
-%build_params.build_weight = 0.01;
-%build_params.memory_weight = 0;
 [index, parameters] = flann_build_index(dataset, build_params);
-[result, dists] = flann_search(index,testset,1000,parameters);
+tic;
+[result, dists] = flann_search(index,testset,500,parameters);
 flann_free_index(index);
 toc;
 
 %%
-n_querys = size(result,2);
-nn_num = size(result,1);
-
-for i = 1:n_querys
-%    figure;
-%    hold on;
-%    plot(testset(1,i),testset(2,i),'ro');
-    for j = 1:nn_num
-        plot(dataset(1,result(j,i)),dataset(2,result(j,i)),'x');
+make_plot_results = 1;
+if(make_plot_results)
+    n_querys = size(result,2);
+    nn_num = size(result,1);
+    hold on;
+    for i = 1:n_querys
+        hold on;
+        plot3(testset(1,i),testset(2,i),testset(3,i),'ro');
+        for j = 1:nn_num
+            plot3(dataset(1,result(j,i)),dataset(2,result(j,i)),dataset(3,result(j,i)),'x');
+        end
+        axis ([0 1 0 1 0 1]);
     end
-    axis ([0 1 0 1]);
-end
 
-figure;
-plot(dists);
+    figure;
+    plot(dists);
+end
